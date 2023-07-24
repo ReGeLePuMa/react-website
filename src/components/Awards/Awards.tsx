b91e9e89118c3f966fb326774d93fb68ad58e92c
ReGeLePuMa <scpetrea@gmail.com>
2023-07-24 19:40:53 +0300

    Fixed Awards section responsivness

--- src/components/Awards/Awards.tsx
+++ src/components/Awards/Awards.tsx
@@ -5,15 +5,16 @@ import { BiMedal } from "react-icons/bi"
 function Awards() {
     return (
         <div id="awards-section" className="flex min-[700px]:flex-row min-[300px]:flex-col h-screen w-screen  bg-white p-4">
-            <div className="min-[700px]:w-1/2 min-[300px]:w-full h-auto  flex flex-col items-center justify-center flex-1">
+            <div className="min-[700px]:w-1/2 min-[300px]:w-full h-auto flex flex-grow flex-col items-center justify-center ">
                 <h1 className="font-bold text-black text-4xl">Awards</h1>
                 <AiOutlineTrophy className="w-1/2 h-1/2
                 min-[900px]:w-80 min-[900px]:h-80
                 min-[700px]:w-60 min-[700px]:h-60
-                min-[300px]:w-52 min-[300px]:h-52" />
+                min-[400px]:w-[200px] min-[400px]:h-[200px]
+                min-[300px]:w-[175px] min-[300px]:h-[175px]" />
             </div>
-            <div className="min-[700px]:w-1/2 min-[300px]:w-full h-auto flex flex-col items-center justify-center">
-                <div className="flex flex-row border-2 border-black w-full rounded-lg my-4 items-center p-4 bg-light-gray">
+            <div className="min-[700px]:w-1/2 min-[300px]:w-full h-auto flex flex-grow flex-col items-center justify-center">
+                <div className="flex flex-row border-2 border-black w-full h-auto rounded-lg mb-4 items-center p-4 bg-light-gray">
                 <BiMedal className="w-16 h-16 mr-4"/>
                 <div className="w-full h-auto">
                     <h2 className="font-bold text-black
@@ -25,12 +26,12 @@ function Awards() {
                     min-[700px]:text-md
                     min-[300px]:text-sm">Congress of the University of Medicine and Pharmacy "Carol Davila" Bucharest X edition</p>
                     <p className="text-gray-500 italic
-                    min-[900px]:text-xl
-                    min-[700px]:text-md
-                    min-[300px]:text-sm">Oral Presentation - CBCT assessment of the greater palatine canal</p>
+                    min-[900px]:text-md
+                    min-[700px]:text-sm
+                    min-[300px]:text-xs">Oral Presentation - CBCT assessment of the greater palatine canal</p>
                 </div>
             </div>
-            <div className="flex border-2 flex-row border-black w-full rounded-lg my-4 items-center p-4 bg-light-gray">
+            <div className="flex border-2 flex-row border-black w-full h-auto rounded-lg  items-center p-4 bg-light-gray">
                     <BiMedal className="w-16 h-16 mr-4"/>
                 <div className="w-full h-auto">
                         <h2 className="font-bold text-black
@@ -42,9 +43,9 @@ function Awards() {
                         min-[700px]:text-md
                         min-[300px]:text-sm">Preclinical Section - Dent-X Congress 3rd edition</p>
                         <p className="text-gray-500 italic
-                        min-[900px]:text-xl
-                        min-[700px]:text-md
-                        min-[300px]:text-sm">Oral Presentation - The Zygomatic Implant: An anatomical view of the potential risks</p>
+                        min-[900px]:text-md
+                        min-[700px]:text-sm
+                        min-[300px]:text-xs">Oral Presentation - The Zygomatic Implant: An anatomical view of the potential risks</p>
                 </div>
             </div>    
             </div>
